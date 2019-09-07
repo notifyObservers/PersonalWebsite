6c7ac5bd2cedf2aba92de1f357d5b9ecd12e766b
notifyObservers <1319344117@qq.com>
2019-09-07 12:56:16 -0400

    - Added info

--- MyWebsite/MyWebsite/Controllers/HomeController.cs
+++ MyWebsite/MyWebsite/Controllers/HomeController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MyWebsite.Models;
+using System.Web;
+using System.IO;
+using System.Data;
 
 namespace MyWebsite.Controllers
 {
@@ -42,9 +45,12 @@ namespace MyWebsite.Controllers
             return View();
         } */
 
-        public IActionResult Privacy()
+        public IActionResult Resume()
         {
-            return View();
+            string path = "C:/Users/Linda Li/DEV/Github/PersonalWebsite/MyWebsite/MyWebsite/Controllers/Resumewinter2020.pdf";
+            var stream = new FileStream(path, FileMode.Open);
+            return new FileStreamResult(stream, "application/pdf");
+            //return File("C:/Users/Linda Li/DEV/Github/PersonalWebsite/MyWebsite/MyWebsite/Controllers/Resumewinter2020.pdf", "Application/pdf");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
